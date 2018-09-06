6e49e7d74bfb89ce4e18cd68acd7c4e323679631
Ben Nortier <ben@bjnortier.com>
2018-09-06 16:32:06 +0200

    minotaur monitor

--- minotaur.py
+++ minotaur.py
@@ -1,4 +1,4 @@
-import os
+import os, sys
 from gym import Wrapper
 from gym.wrappers import Monitor
 from gym.wrappers.monitoring import video_recorder
@@ -7,20 +7,32 @@ from dotenv import load_dotenv, find_dotenv
 from git import Repo
 import numpy as np
 
-load_dotenv(find_dotenv('.minotaur'))
-jwt = os.getenv("MINOTAUR_JWT")
-assert jwt
+# load_dotenv(find_dotenv('.minotaur'))
+# jwt = os.getenv("MINOTAUR_JWT")
+# assert jwt
+
+# minotaur_url = 'https://minotaur-1.herokuapp.com'
+# jwt = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOnsidXNlcklkIjoxfSwiaWF0IjoxNTM2MTU4NjMyfQ.16ScqXXRaU847_qStKKfPTMR8_NTGIl66O5p7uUkAu8'
+
+minotaur_url = 'http://localhost:8100'
+jwt = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOnsidXNlcklkIjoxfSwiaWF0IjoxNTM1MzgxNTcwfQ.3YfkD5LbR3BrhGbLmGIujd4byFkh7s9Gku03BCZ7eO0'
 
 def create_minotaur_experiment():
     repo = Repo(os.path.dirname(__file__))
+    is_dirty = repo.is_dirty()
+    is_master = repo.refs.master.commit.hexsha == repo.head.reference.commit.hexsha
+    is_master_synced = repo.refs.master.commit.hexsha == repo.remotes.origin.refs.master.commit.hexsha
     headers = {'Authorization': 'Bearer {0}'.format(jwt)}
     experiment_spec = {
         'name': 'Odie-v2',
         'meta': {},
         'repo': list(repo.remotes.origin.urls)[0],
-        'commit': repo.head.reference.commit.hexsha
+        'commit': repo.head.reference.commit.hexsha,
+        'isClean': not is_dirty,
+        'isMaster': is_master,
+        'isMasterSynced': is_master_synced
     }
-    response = requests.post('http://localhost:8100/api/experiments', headers=headers, json=experiment_spec)
+    response = requests.post('{}/api/experiments'.format(minotaur_url), headers=headers, json=experiment_spec)
     print('created experiment: "{0}"'.format(response.json()['name']))
     return response.json()['id']
 
@@ -44,22 +56,27 @@ class MinotaurMonitor(Wrapper):
         observation, reward, done, info = self.env.step(action)
         self.episode_rewards.append(reward)
         if done:
-            headers = {'Authorization': 'Bearer {0}'.format(jwt)}
-            # Upload the episode reward mean
-            epsode_total_reward = np.sum(np.array(self.episode_rewards))
-            response = requests.post(
-                'http://localhost:8100/api/data/{0}'.format(self.experiment_id),
-                headers=headers,
-                json={'episode_total_reward': episode_total_reward})
-            print('posting metrics: {}:{}'.format(response.status_code, response.content))
-            # Upload the video recording
-            self.video_recorder.close()
-            filename = '{}.mp4'.format(self.base_path)
-            response = requests.post(
-               'http://localhost:8100/api/data/{0}'.format(self.experiment_id),
-               headers=headers,
-               files={'animation': open(filename, 'rb')})
-            print('posting video: {}:{}'.format(response.status_code, response.content))
+            try:
+                headers = {'Authorization': 'Bearer {0}'.format(jwt)}
+                # Upload the episode reward mean
+                episode_total_reward = np.sum(np.array(self.episode_rewards))
+                response = requests.post(
+                    '{}/api/data/{}'.format(minotaur_url, self.experiment_id),
+                    headers=headers,
+                    json={'episode': self.episode_id, 'episode_total_reward': episode_total_reward})
+                print('posting metrics: {}:{}'.format(response.status_code, response.content))
+                # Upload the video recording
+                self.video_recorder.close()
+                if (self.video_callable(self.episode_id)):
+                    filename = '{}.mp4'.format(self.base_path)
+                    response = requests.post(
+                       '{}/api/data/{}'.format(minotaur_url, self.experiment_id),
+                       headers=headers,
+                       data={'episode': self.episode_id},
+                       files={'animation': open(filename, 'rb')})
+                    print('posting video: {}:{}'.format(response.status_code, response.content))
+            except:
+                print('[ERR]', sys.exc_info())
             # Prepare for next episode
             self.episode_rewards = []
             self.episode_id += 1
@@ -74,7 +91,7 @@ class MinotaurMonitor(Wrapper):
             env=self.env,
             base_path=self.base_path,
             metadata={'episode_id': self.episode_id},
-            enabled=True
+            enabled=self.video_callable(self.episode_id)
         )
         self.video_recorder.capture_frame()
 
